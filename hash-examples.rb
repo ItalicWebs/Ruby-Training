99a8fb418e45edd25fefd6f6bef14696c42c23f1
ItalicWebs <josepartida.rh@gmail.com>
2020-02-17 18:02:22 -0800

    Converted hashes into symbols and cleaned

--- hash-examples.rb
+++ hash-examples.rb
@@ -1,45 +1,45 @@
 online_orders = {
-	"order_abc1" => {
-		'item_number': 'it0001', 
-		'order_date': '2020-02-10', 
-		'order_status': 'arrived', 
-		'order_arrival_date': '2020-02-12', 
-		'order_tracking_info': '390344839629',
-		'order_associated_customer': 'cid_001'},
-	"order_abc2" => {
-		'item_number': 'it0005', 
-		'order_date': '2020-02-15', 
-		'order_status': 'shipping', 
-		'order_arrival_date': '2020-02-15', 
-		'order_tracking_info': 'pending',
-		'order_associated_customer': 'cid_002'},
-	"order_abc3" => {
-		'item_number': 'it0025', 
-		'order_date': '2020-02-16', 
-		'order_status': 'pending credit', 
-		'order_arrival_date': 'pending', 
-		'order_tracking_info': 'pending',
-		'order_associated_customer': 'cid_002'},
+	'order_abc1' => {
+		item_number: 'it0001', 
+		order_date: '2020-02-10', 
+		order_status: 'arrived', 
+		order_arrival_date: '2020-02-12', 
+		order_tracking_info: '390344839629',
+		order_associated_customer: 'cid_001'},
+	'order_abc2' => {
+		item_number: 'it0005', 
+		order_date: '2020-02-15', 
+		order_status: 'shipping', 
+		order_arrival_date: '2020-02-15', 
+		order_tracking_info: 'pending',
+		order_associated_customer: 'cid_002'},
+	'order_abc3' => {
+		item_number: 'it0025', 
+		order_date: '2020-02-16', 
+		order_status: 'pending credit', 
+		order_arrival_date: 'pending', 
+		order_tracking_info: 'pending',
+		order_associated_customer: 'cid_002'},
 }
 
 # puts online_orders["order_abc1"].keys
 # puts online_orders["order_abc1"][:order_associated_customer]
 
 account_rep = {
-	"ar_001" => {
-		'ar_first_name': 'James',
-		'ar_last_name': 'Hobo', 
-		'ar_company_title': 'Development',
-		'ar_contact_phone_1': '222-222-2222',
-		'ar_contact_phone_2': '0',
-		'ar_contact_email_1': 'hobo@italicwebs.com'},
-	"ar_002" => {
-		'ar_first_name': 'Jose', 
-		'ar_last_name': 'Polar', 
-		'ar_company_title': 'Development',
-		'ar_contact_phone_1': '333-333-3333', 
-		'ar_contact_phone_2': '0', 
-		'ar_contact_email_1': 'jose.polar@italicwebs.com'},
+	'ar_001' => {
+		ar_first_name: 'James',
+		ar_last_name: 'Hobo', 
+		ar_company_title: 'Development',
+		ar_contact_phone_1: '222-222-2222',
+		ar_contact_phone_2: '0',
+		ar_contact_email_1: 'hobo@italicwebs.com'},
+	'ar_002' => {
+		ar_first_name: 'Jose', 
+		ar_last_name: 'Polar', 
+		ar_company_title: 'Development',
+		ar_contact_phone_1: '333-333-3333', 
+		ar_contact_phone_2: '0', 
+		ar_contact_email_1: 'jose.polar@italicwebs.com'},
 		
 }
 
@@ -47,28 +47,28 @@ account_rep = {
 # puts account_rep["ar_002"][:ar_last_name]
 
 customer_information = {
-	"cid_001" => {
-		'ar_first_name': 'John',
-		'ar_middle_name': 'P.',
-		'ar_last_name': 'Doe',
-		'ar_address_street_1': 'P.O Box 1001',
-		'ar_address_street_2': 'Att: John',
-		'ar_address_city': 'Fresno',
-		'ar_address_state': 'CA',
-		'ar_address_zip': '93702',
-		'ar_address_country': 'United States',
-		'ar_account_rep': account_rep["ar_001"]},
-	"cid_002" => {
-		'ar_first_name': 'Jane',
-		'ar_middle_name': 'P.',
-		'ar_last_name': 'Doe',
-		'ar_address_street_1': 'P.O Box 1002',
-		'ar_address_street_2': 'Att: Jane',
-		'ar_address_city': 'Clovis',
-		'ar_address_state': 'CA',
-		'ar_address_zip': '93702',
-		'ar_address_country': 'United States',
-		'ar_account_rep': account_rep["ar_002"]},
+	'cid_001' => {
+		ar_first_name: 'John',
+		ar_middle_name: 'P.',
+		ar_last_name: 'Doe',
+		ar_address_street_1: 'P.O Box 1001',
+		ar_address_street_2: 'Att: John',
+		ar_address_city: 'Fresno',
+		ar_address_state: 'CA',
+		ar_address_zip: '93702',
+		ar_address_country: 'United States',
+		ar_account_rep: account_rep["ar_001"]},
+	'cid_002' => {
+		ar_first_name: 'Jane',
+		ar_middle_name: 'P.',
+		ar_last_name: 'Doe',
+		ar_address_street_1: 'P.O Box 1002',
+		ar_address_street_2: 'Att: Jane',
+		ar_address_city: 'Clovis',
+		ar_address_state: 'CA',
+		ar_address_zip: '93702',
+		ar_address_country: 'United States',
+		ar_account_rep: account_rep["ar_002"]},
 }
 
 puts "-- Welcome to our order tracker --"
